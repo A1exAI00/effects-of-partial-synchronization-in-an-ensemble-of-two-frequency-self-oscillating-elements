757e7250cc85c1f5c17471655de92c55a8e591ea
A1exAI00 <lksndrakinin@gmail.com>
2024-03-07 12:44:58 +0300

    Refactor to use src article1 functionality (atricle1_calc_final_phase)

---
+++ scripts/08-article1_phase_from_d_depend.jl
@@ -0,0 +1,78 @@
+#=
+Цель программы: закономерности смены режимов системв при изменении силы связи d.
+
+Выбрать НУ соответственно последовательности 1000110, фазы распределить случайно.
+Для каждого значения d итегрировать систему N_repeat раз, каждый раз записывать 
+среднюю амплитуду, частоту и фазу i-го осциллятора в цепочке, потом усреднить.
+
+TODO: уточнить описание программы, что здесь зависимость конечной фазы от параметра d
+=#
+
+#########################################################################################
+
+using DrWatson
+@quickactivate "semester8"
+
+using CairoMakie
+
+include(srcdir("article1.jl"))
+include(srcdir("misc.jl"))
+
+#########################################################################################
+
+a, b, c, ε = 0.32, 0.79, 1.166, 0.001
+N_elements = 7
+
+d_start, d_end, d_N = 0.0, 0.05, 100
+d_range = range(d_start, d_end, d_N)
+
+initial_pattern = [true, false, false, false, true, true, false]
+
+u₀ = @. c * initial_pattern + a * !initial_pattern
+v₀ = @. 0.0 * ones(Float64, N_elements)
+
+t_start, t_end = 0.0, 5e4
+
+#########################################################################################
+
+U₀ = [u₀..., v₀...]
+random_initial_offset = 2 .* (rand(2*N_elements) .- 0.5) .* 0.05
+U₀ += random_initial_offset
+t_span = [t_start, t_end]
+
+#########################################################################################
+
+φᵢ_from_d = []
+for (i,d) in enumerate(d_range)
+    println(i)
+    p = (a, b, c, ε, d, N_elements)
+    φᵢ = atricle1_calc_final_phase(U₀, p, t_span)
+    φ₅ = φᵢ[5]
+    φᵢ = rem2pi.(φᵢ .- φ₅, RoundNearest) # .- π
+    push!(φᵢ_from_d, φᵢ)
+end
+
+#########################################################################################
+
+fig = Figure(size=(1000, 700))
+ax = Axis(fig[1, 1], 
+    title="Зависимость конечной фазы элементов цепочки от параметра d", 
+    xlabel="d", 
+    ylabel="φᵢ", 
+	xminorticksvisible = true, 
+	xminorgridvisible = true, 
+	yminorticksvisible = true, 
+	yminorgridvisible = true, 
+	xminorticks = IntervalsBetween(10),
+	yminorticks = IntervalsBetween(10)
+)
+
+vlines!(ax, 0.0, color=:black)
+hlines!(ax, 0.0, color=:black)
+
+for i in 1:N_elements
+    scatter!(ax, d_range, [φᵢ_from_d[j][i] for j in eachindex(d_range)], label="φ_$(i) - φ₅")
+end
+
+axislegend(ax, position=:rb) # (l, r, c), (b, t, c)
+save(plotsdir("08-article1_phase_from_d_depent_$(time_ns()).png"), fig, px_per_unit=2)
